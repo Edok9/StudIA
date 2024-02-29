cfc2e77e40af09599b07823cb65547ee955bfb42
Sebastian Parraguez <sparraguez@sclconsultores.com>
2024-02-29 11:50:01 -0300

    Mas cambios de labels

--- solicitudesManager/models.py
+++ solicitudesManager/models.py
@@ -3,9 +3,9 @@ from loginApp.models import Usuario
     
 class Tipo_Solicitud(models.Model):
     id_tipo_sol = models.AutoField(primary_key=True)
-    descripcion_t_sol = models.CharField(max_length=70)
-    nombre_t_sol = models.CharField(max_length=25)
-    estado_sol = models.CharField(max_length=1)
+    descripcion_t_sol = models.CharField(max_length=70, verbose_name = "Descripción")
+    nombre_t_sol = models.CharField(max_length=25, verbose_name = "Nombre")
+    estado_sol = models.CharField(max_length=1, verbose_name = "Estado")
 
     class Meta:
         verbose_name = "Tipo de Solicitud"
@@ -13,12 +13,12 @@ class Tipo_Solicitud(models.Model):
    
 class Solicitud(models.Model):
     id_sol = models.AutoField(primary_key=True)
-    nombre_sol = models.CharField(max_length=30)
-    tipo_sol = models.CharField(max_length=30)
-    descripcion_sol = models.CharField(max_length=70)
-    created_at = models.DateTimeField()
-    id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    id_tipo_sol = models.OneToOneField(Tipo_Solicitud, on_delete=models.CASCADE)
+    nombre_sol = models.CharField(max_length=30, verbose_name = "Nombre")
+    tipo_sol = models.CharField(max_length=30, verbose_name = "Tipo")
+    descripcion_sol = models.CharField(max_length=70, verbose_name = "Descripción")
+    created_at = models.DateTimeField(verbose_name = "Fecha de Creación")
+    id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, verbose_name = "Usuario")
+    id_tipo_sol = models.OneToOneField(Tipo_Solicitud, on_delete=models.CASCADE, verbose_name = "Tipo de Solicitud")
 
     class Meta:
         verbose_name = "Solicitud"
